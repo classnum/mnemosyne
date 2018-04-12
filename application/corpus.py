477fa9fc82a4ba863513ceb94c7967d800ad4664
Aurélien Berra <aurelien.berra@gmail.com>
2018-04-12 11:43:34 +0200

    test corpus def

--- application/corpus.py
+++ application/corpus.py
@@ -1,31 +1,24 @@
 from MyCapytain.resources.prototypes.cts.inventory import CtsTextInventoryCollection, CtsTextInventoryMetadata
 from MyCapytain.resolvers.utils import CollectionDispatcher
 
-
 from capitains_nautilus.cts.resolver import NautilusCTSResolver
 
 # Setting up the collections
 
 general_collection = CtsTextInventoryCollection()
-texts = CtsTextInventoryMetadata("collection", parent=general_collection)
-texts.set_label("Texts", "eng")
-texts.set_label("Textes", "fre")
+poetry = CtsTextInventoryMetadata("poetry_collection", parent=general_collection)
+poetry.set_label("Poetry", "eng")
+poetry.set_label("Poésie", "fre")
 
 # priapeia = CtsTextInventoryMetadata("priapeia_collection", parent=general_collection)
 # priapeia.set_label("Priapeia", "eng")
 # priapeia.set_label("Priapées", "fre")
 
-hchn = CtsTextInventoryMetadata("id:hchn", parent=general_collection)
-hchn.set_label("HCHN", "eng")
-hchn.set_label("HCHN", "fre")
-organizer = CollectionDispatcher(general_collection, default_inventory_name="id:hchn")
+misc = CtsTextInventoryMetadata("id:misc", parent=general_collection)
+misc.set_label("Miscellaneous", "eng")
+misc.set_label("Textes Divers", "fre")
+organizer = CollectionDispatcher(general_collection, default_inventory_name="id:misc")
 
-
-@organizer.inventory("hchn")
-def organize_my_hchn(collection, path=None, **kwargs):
-    if not collection.id.startswith("urn:cts:latinLit:phi9999"):
-        return True
-    return False
 
 # @organizer.inventory("priapeia_collection")
 # def organize_my_priapeia(collection, path=None, **kwargs):
@@ -34,10 +27,10 @@ def organize_my_hchn(collection, path=None, **kwargs):
 #     return False
 
 
-@organizer.inventory("collection")
-def organize_my_collection(collection, path=None, **kwargs):
+@organizer.inventory("poetry_collection")
+def organize_my_poetry(collection, path=None, **kwargs):
     # If we are not dealing with Priapeia
-    if not collection.id.startswith("urn:cts:latinLit:phi9999"):
+    if not collection.id.startswith("urn:cts:latinLit:phi1103"):
         # Textgroups have a wonderful shortcut to their editions and translations : .readableDescendants
         for text in collection.readableDescendants:
             for citation in text.citation:
@@ -45,10 +38,27 @@ def organize_my_collection(collection, path=None, **kwargs):
                     return True
     return False
 
-# @organizer.inventory("poetry_collection")
-# def organize_my_poetry(collection, path=None, **kwargs):
+# # TEST
+# general_collection = CtsTextInventoryCollection()
+# texts = CtsTextInventoryMetadata("collection", parent=general_collection)
+# texts.set_label("Texts", "eng")
+# texts.set_label("Textes", "fre")
+
+# hchn = CtsTextInventoryMetadata("id:hchn", parent=general_collection)
+# hchn.set_label("HCHN", "eng")
+# hchn.set_label("HCHN", "fre")
+# organizer = CollectionDispatcher(general_collection, default_inventory_name="id:hchn")
+
+# @organizer.inventory("hchn")
+# def organize_my_hchn(collection, path=None, **kwargs):
+#     if not collection.id.startswith("urn:cts:latinLit:phi9999"):
+#         return True
+#     return False
+
+# @organizer.inventory("collection")
+# def organize_my_collection(collection, path=None, **kwargs):
 #     # If we are not dealing with Priapeia
-#     if not collection.id.startswith("urn:cts:latinLit:phi1103"):
+#     if not collection.id.startswith("urn:cts:latinLit:phi9999"):
 #         # Textgroups have a wonderful shortcut to their editions and translations : .readableDescendants
 #         for text in collection.readableDescendants:
 #             for citation in text.citation:
@@ -58,5 +68,5 @@ def organize_my_collection(collection, path=None, **kwargs):
 
 
 # Parsing the data
-resolver = NautilusCTSResolver(["corpora/hchn"], dispatcher=organizer)
+resolver = NautilusCTSResolver(["corpora/hchn", "corpora/priapeia"], dispatcher=organizer)
 resolver.parse()
