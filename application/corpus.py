3c7da2d556418bb7e3e4a497c47fdb45cefeb00d
Aurélien Berra <aurelien.berra@gmail.com>
2018-04-12 13:04:57 +0200

    test corpus name

--- application/corpus.py
+++ application/corpus.py
@@ -6,18 +6,18 @@ from capitains_nautilus.cts.resolver import NautilusCTSResolver
 # Setting up the collections
 
 general_collection = CtsTextInventoryCollection()
-poetry = CtsTextInventoryMetadata("poetry_collection", parent=general_collection)
-poetry.set_label("Poetry", "eng")
-poetry.set_label("Poésie", "fre")
+# poetry = CtsTextInventoryMetadata("poetry_collection", parent=general_collection)
+# poetry.set_label("Poetry", "eng")
+# poetry.set_label("Poésie", "fre")
 
 # priapeia = CtsTextInventoryMetadata("priapeia_collection", parent=general_collection)
 # priapeia.set_label("Priapeia", "eng")
 # priapeia.set_label("Priapées", "fre")
 
-misc = CtsTextInventoryMetadata("id:misc", parent=general_collection)
-misc.set_label("HCHN", "eng")
-misc.set_label("HCHN", "fre")
-organizer = CollectionDispatcher(general_collection, default_inventory_name="id:misc")
+hchn = CtsTextInventoryMetadata("hchn", parent=general_collection)
+hchn.set_label("HCHN", "eng")
+hchn.set_label("HCHN", "fre")
+organizer = CollectionDispatcher(general_collection, default_inventory_name="hchn")
 
 
 # @organizer.inventory("priapeia_collection")
@@ -27,16 +27,16 @@ organizer = CollectionDispatcher(general_collection, default_inventory_name="id:
 #     return False
 
 
-@organizer.inventory("poetry_collection")
-def organize_my_poetry(collection, path=None, **kwargs):
-    # If we are not dealing with Priapeia
-    if not collection.id.startswith("urn:cts:latinLit:phi1103"):
-        # Textgroups have a wonderful shortcut to their editions and translations : .readableDescendants
-        for text in collection.readableDescendants:
-            for citation in text.citation:
-                if citation.name == "line":
-                    return True
-    return False
+# @organizer.inventory("poetry_collection")
+# def organize_my_poetry(collection, path=None, **kwargs):
+#     # If we are not dealing with Priapeia
+#     if not collection.id.startswith("urn:cts:latinLit:phi1103"):
+#         # Textgroups have a wonderful shortcut to their editions and translations : .readableDescendants
+#         for text in collection.readableDescendants:
+#             for citation in text.citation:
+#                 if citation.name == "line":
+#                     return True
+#     return False
 
 # # TEST
 # general_collection = CtsTextInventoryCollection()
