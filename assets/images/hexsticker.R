f9dff773203776f8345a5ce08079528df61b5064
Aurélien Berra <aurelien.berra@gmail.com>
2018-10-28 02:10:56 +0200

    test hexsticker as logo

---
+++ assets/images/hexsticker.R
@@ -0,0 +1,18 @@
+
+# https://github.com/GuangchuangYu/hexSticker -----------------------------
+
+hexSticker::sticker("scrollicon.png",
+                    package="Mnemosyne",
+                    spotlight = FALSE,
+                    h_size = 1.5,
+                    h_color = "#337ab7", # "gold",
+                    h_fill = "white", # "gold",
+                    p_size=6, p_y = 1.3,
+                    p_color="#337ab7",
+                    # p_family = "Source Code Pro",
+                    s_x=1, s_y=0.7, s_width=0.3,
+                    url="https://classnum-mnemosyne.herokuapp.com",
+                    u_size = 1,
+                    u_y = 0.1,
+                    u_color = "#337ab7",
+                    filename="mnemosyne.png")
